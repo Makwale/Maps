13b728c2ad0cb31451dba85ef552b4038d72b2ab
Makwale <makwale.em@gmail.com>
2020-11-30 22:36:46 +0200

    second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,7 @@ declare var mapboxgl: any;
 })
 export class HomePage {
   map: any;
+  index:number = 0;
 
   // @ViewChild('map', {read: ElementRef, static: true, })
   // mapRef: ElementRef;
@@ -18,20 +19,101 @@ export class HomePage {
   constructor() {}
 
   ionViewDidEnter(){
-    console.log("Hello world");
-    this.showMap();
+    this.map = this.showMap();
+
+    let marker = new mapboxgl.Marker({
+      color:"red"
+    })
+    .setLngLat([28.02, -25.57])
+    .addTo(this.map);
+
+    let marker2 = new mapboxgl.Marker({color: "lightgreen"})
+    .setLngLat([30.50, -20.181])
+    .addTo(this.map);
+
+    let marker3 = new mapboxgl.Marker({color: "orange"})
+    .setLngLat([28.188, -25.7461])
+    .addTo(this.map);
+
   }
 
   showMap(){
     mapboxgl.accessToken = 'pk.eyJ1IjoibWFudWVsbWFrd2FsZSIsImEiOiJja2hsc3lmYWUyZzRnMnRsNnY2NWIyeGR6In0.1MGnfpXj_dV2QBO3SchfqA';
-    var map = new mapboxgl.Map({
+    let map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-74.5, 40],
-        zoom: 8
+        style: 'mapbox://styles/mapbox/navigation-preview-day-v4',
+        center: [25.34, -30.181],
+        zoom: 4,
     });
 
-    map.addControl(new mapboxgl.NavigationControl());
+    map.addControl(
+    new MapboxDirections({
+      accessToken: mapboxgl.accessToken
+    }),
+      'bottom-left'
+    );
+
+    map.addControl(
+    new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      marker: {
+        color: "Red"
+      }
+      })
+    );
+
+    map.addControl(new mapboxgl.NavigationControl({
+      showCompass: true,
+      showZoom: true
+    }));
+
+    let geolocate = new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true,
+        showUserLocation: true,
+        showAccuracyCircle: true
+    })
+
+    map.addControl(geolocate);
+    let index = 0;
+    geolocate.on('geolocate', function() {
+      index++;
+      console.log("Location changed ",index);
+    });
+
+    var markerHeight = 50, markerRadius = 10, linearOffset = 25;
+    var popupOffsets = {
+     'top': [0, 0],
+     'top-left': [0,0],
+     'top-right': [0,0],
+     'bottom': [0, -markerHeight],
+     'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+     'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+     'left': [markerRadius, (markerHeight - markerRadius) * -1],
+     'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+     };
+
+    var popup = new mapboxgl.Popup({offset: popupOffsets})
+      .setLngLat([28.02, -25.57])
+      .setHTML("<h1>Hello World!</h1>")
+      .setMaxWidth("300px")
+      .addTo(map);
+
+    return map;
+  }
+
+  test(){
+    let marker = new mapboxgl.Marker({
+      color:"gray"
+    })
+    .setLngLat([25.34, -30.181])
+    .addTo(this.map);
+
+    this.map = this.showMap('mapbox://styles/mapbox/streets-v11');
+
   }
 
 }
